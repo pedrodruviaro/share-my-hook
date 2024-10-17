96462982328f923b231f157c191efa06cf444c95
pedrodruviaro <pedrodruviaro@gmail.com>
2024-10-17 11:22:20 -0300

    feat: add edit profile screen

---
+++ components/Profile/Edit/Form.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const emits = defineEmits<{
+  (e: "save-profile"): void
+}>()
+</script>
+
+<template>
+  <form class="space-y-2" @submit.prevent="emits('save-profile')">
+    <div class="grid gap-2 md:grid-cols-2">
+      <UFormGroup label="Nome" required>
+        <UInput placeholder="John Doe" icon="i-heroicons-user" />
+      </UFormGroup>
+
+      <UFormGroup label="Cargo">
+        <UInput
+          placeholder="Fullstack Developer @ GitHub"
+          icon="i-heroicons-briefcase"
+        />
+      </UFormGroup>
+    </div>
+
+    <UFormGroup label="Fale um pouco sobre você">
+      <UTextarea :rows="6" />
+    </UFormGroup>
+
+    <UButton
+      type="submit"
+      label="Salvar"
+      icon="i-heroicons-check"
+      variant="outline"
+    />
+  </form>
+</template>
--- pages/dashboard/profile/edit.vue
+++ pages/dashboard/profile/edit.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const handleSaveProfile = () => {
+  console.log("* Salvar perfil")
+}
+
+useSeoMeta({
+  title: "Editar perfil",
+})
+</script>
 
 <template>
-  <div>
-    <h1>Profile edit</h1>
+  <div class="grid gap-8">
+    <h1 class="font-bold text-xl md:text-2xl">Edite sua informações</h1>
+
+    <ProfileEditForm @save-profile="handleSaveProfile" />
   </div>
 </template>
