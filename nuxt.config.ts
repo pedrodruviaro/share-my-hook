f7ce962a39b4765dafb1b638e17cb23bf973e805
pedrodruviaro <pedrodruviaro@gmail.com>
2024-10-17 08:27:25 -0300

    feat: :heavy_plus_sign: add nuxtUi module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
+  colorMode: {
+    preference: "dark",
+    fallback: "dark",
+    classPrefix: "",
+    classSuffix: "-mode",
+    storageKey: "sharemyhook-color-mode",
+  },
 })
