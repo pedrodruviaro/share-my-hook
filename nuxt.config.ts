b602fa1c324c95657c809ccac3fc9a5b9fd8b4ec
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-06 15:17:45 -0300

    fix: :bug: fix route exclude rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: "/auth/login",
       callback: "/auth/redirect",
-      exclude: ["/", "/public/*"],
+      exclude: ["/", "/hook/*", "/users/*"],
     },
   },
   colorMode: {
