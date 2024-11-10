a1250170c83e220e6c08654ff8f2bad61081da90
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-10 08:57:10 -0300

    refactor: remove unused component

--- components/Hook/Public/Documentation.vue
+++ components/Hook/Public/Documentation.vue
@@ -7,7 +7,11 @@ const parsedDescription = useMarkdown(props.documentation)
 </script>
 
 <template>
-  <BaseWidget title="Como usar">
+  <UCard>
+    <template #header>
+      <h2 class="font-bold text-xl md:text-2xl">Como usar</h2>
+    </template>
+
     <HookProse :html="parsedDescription" />
-  </BaseWidget>
+  </UCard>
 </template>
