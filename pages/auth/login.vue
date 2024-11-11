a88fc760d1b84848b1ed9fe34c7b5315d7a47e06
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-11 08:08:51 -0300

    feat: change auth login page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -13,16 +13,27 @@ const { isLogged } = storeToRefs(useUserStore())
 const router = useRouter()
 
 onMounted(() => {
-  if (isLogged.value) {
-    router.push("/dashboard")
-  }
+  if (isLogged.value) router.push("/dashboard")
 })
 </script>
 
 <template>
   <BaseLayoutWrapper>
+    <template #header>
+      <header class="py-4 border-b border-slate-200 dark:border-slate-700">
+        <UContainer>
+          <div class="flex flex-wrap gap-2 justify-between">
+            <Logo />
+            <DarkModeToggler />
+          </div>
+        </UContainer>
+      </header>
+    </template>
+
     <UContainer>
-      <div class="w-full max-w-[400px] flex justify-center mx-auto">
+      <div
+        class="w-full max-w-[400px] flex justify-center mx-auto pt-10 lg:pt-20"
+      >
         <AuthLoginForm
           :loading
           :isLogged="false"
